7f430706c2e82dd9bd552f5f3cbbb93b19044359
Gregory Topscher <126685397+cozyfestiv@users.noreply.github.com>
2024-01-31 19:45:24 -0500

    cleaning up intro copy

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -10,13 +10,11 @@ const Intro = () => {
         <span className="hello">Hi,</span>
         <span className="intro-text">
           I'm <span className="intro-name">Greg</span>
-          <br />
-          <span className="intro-title">Software Developer</span>
         </span>
         <p className="intro-p">
-          I am a software developer with a background in art and writing. My
-          mission is to write sustainable code for the health of our planet and
-          the people on it.{" "}
+          I'm a <span className="intro-title">Software Developer</span> <br />{" "}
+          with a background in art and writing. My mission is to write
+          sustainable code for the health of our planet and the people on it.{" "}
         </p>
         {/* <Link>
           <button className="btn">Hire Me</button>
