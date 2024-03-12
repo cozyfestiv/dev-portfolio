79e2730b0cf6a53ffb3eb636005ebd92b60d23da
Gregory Topscher <126685397+cozyfestiv@users.noreply.github.com>
2024-03-12 11:33:53 -0400

    added header tag and removed unused code

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,22 +7,77 @@ import { Link } from "react-scroll";
 export function NavBar() {
   const [showMenu, setShowMenu] = useState(false);
   return (
-    <nav className="navbar">
-      <div className="logo-wrapper">
-        <Link
-          to="intro"
-          spy={true}
-          smooth={true}
-          offset={-100}
-          duration={500}
-          className="logo-link"
+    <header>
+      <nav className="navbar">
+        <div className="logo-wrapper">
+          <Link
+            to="intro"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+            className="logo-link"
+          >
+            <img src={logo} alt="Logo" className="logo" />
+            <img src={mobileLogo} alt="Logo" className="mobile-logo" />
+          </Link>
+        </div>
+        <ul className="desktop-menu">
+          <li className="desktop-menu-list-item">
+            <Link
+              activeClass="active"
+              to="intro"
+              spy={true}
+              smooth={true}
+              offset={-100}
+              duration={500}
+            >
+              Home
+            </Link>
+          </li>
+          <li className="desktop-menu-list-item">
+            <Link
+              activeClass="active"
+              to="skills"
+              spy={true}
+              smooth={true}
+              offset={-60}
+              duration={500}
+            >
+              Stack
+            </Link>
+          </li>
+          <li className="desktop-menu-list-item">
+            <Link
+              activeClass="active"
+              to="projects"
+              spy={true}
+              smooth={true}
+              offset={-60}
+              duration={500}
+            >
+              Projects
+            </Link>
+          </li>
+        </ul>
+        <button
+          className="desktop-menu-btn"
+          onClick={() => {
+            document
+              .getElementById("contact")
+              .scrollIntoView({ behavior: "smooth" });
+          }}
         >
-          <img src={logo} alt="Logo" className="logo" />
-          <img src={mobileLogo} alt="Logo" className="mobile-logo" />
-        </Link>
-      </div>
-      <ul className="desktop-menu">
-        <li className="desktop-menu-list-item">
+          <img src="" alt="" className="desktop-menu-img" /> Contact Me
+        </button>
+        <i
+          className="fas fa-bars burger"
+          onClick={() => setShowMenu(!showMenu)}
+        />
+        <div
+          className="mobile-menu"
+          style={{ display: showMenu ? "flex" : "none" }}
+        >
           <Link
             activeClass="active"
             to="intro"
@@ -30,11 +85,11 @@ export function NavBar() {
             smooth={true}
             offset={-100}
             duration={500}
+            className="list-item"
+            onClick={() => setShowMenu(false)}
           >
             Home
           </Link>
-        </li>
-        <li className="desktop-menu-list-item">
           <Link
             activeClass="active"
             to="skills"
@@ -42,11 +97,11 @@ export function NavBar() {
             smooth={true}
             offset={-60}
             duration={500}
+            className="list-item"
+            onClick={() => setShowMenu(false)}
           >
             Stack
           </Link>
-        </li>
-        <li className="desktop-menu-list-item">
           <Link
             activeClass="active"
             to="projects"
@@ -54,77 +109,12 @@ export function NavBar() {
             smooth={true}
             offset={-60}
             duration={500}
+            className="list-item"
+            onClick={() => setShowMenu(false)}
           >
             Projects
           </Link>
-        </li>
-        {/* <Link
-          activeClass="active"
-          to=""
-          spy={true}
-          smooth={true}
-          offset={-60}
-          duration={500}
-          className="desktop-menu-list-item"
-        >
-          Clients
-        </Link> */}
-      </ul>
-      <button
-        className="desktop-menu-btn"
-        onClick={() => {
-          document
-            .getElementById("contact")
-            .scrollIntoView({ behavior: "smooth" });
-        }}
-      >
-        <img src="" alt="" className="desktop-menu-img" /> Contact Me
-      </button>
-      <i
-        className="fas fa-bars burger"
-        onClick={() => setShowMenu(!showMenu)}
-      />
-      <div
-        className="mobile-menu"
-        style={{ display: showMenu ? "flex" : "none" }}
-      >
-        <Link
-          activeClass="active"
-          to="intro"
-          spy={true}
-          smooth={true}
-          offset={-100}
-          duration={500}
-          className="list-item"
-          onClick={() => setShowMenu(false)}
-        >
-          Home
-        </Link>
-        <Link
-          activeClass="active"
-          to="skills"
-          spy={true}
-          smooth={true}
-          offset={-60}
-          duration={500}
-          className="list-item"
-          onClick={() => setShowMenu(false)}
-        >
-          Stack
-        </Link>
-        <Link
-          activeClass="active"
-          to="projects"
-          spy={true}
-          smooth={true}
-          offset={-60}
-          duration={500}
-          className="list-item"
-          onClick={() => setShowMenu(false)}
-        >
-          Projects
-        </Link>
-        {/* <Link
+          {/* <Link
           activeClass="active"
           to=""
           spy={true}
@@ -136,19 +126,20 @@ export function NavBar() {
         >
           Clients
         </Link> */}
-        <Link
-          activeClass="active"
-          to="contact"
-          spy={true}
-          smooth={true}
-          offset={-60}
-          duration={500}
-          className="list-item"
-          onClick={() => setShowMenu(false)}
-        >
-          Contact
-        </Link>
-      </div>
-    </nav>
+          <Link
+            activeClass="active"
+            to="contact"
+            spy={true}
+            smooth={true}
+            offset={-60}
+            duration={500}
+            className="list-item"
+            onClick={() => setShowMenu(false)}
+          >
+            Contact
+          </Link>
+        </div>
+      </nav>
+    </header>
   );
 }
